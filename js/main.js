732300b1aec9edac795b2dd92ba1a273a4f933f0
Qinissoo <122180393+Qinissoo@users.noreply.github.com>
2023-01-07 23:25:17 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+})(jQuery);
+
+// main.js from Defolio //
+jQuery(document).ready(function($) {
+
+
+    var mastheadheight = $('.ds-header').outerHeight();
+    //console.log(mastheadheight);
+    $(".ds-banner,.ds-main-section").css("margin-top" , mastheadheight);
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 10) {
+            $('.ds-header').addClass('ds-fixed-header');
+        }
+        else {
+            $('.ds-header').removeClass('ds-fixed-header');
+        }
+    }).scroll();
+
+
+});
